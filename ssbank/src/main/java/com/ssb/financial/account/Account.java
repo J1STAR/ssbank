1e1217a0809e2dd5dab1bb1ee15906d6f1d998a0
mjbang <mjbang@mjmjbang>
2018-03-02 17:45:40 +0900

    0302 계좌리스트/상세/해지페이지

--- ssbank/src/main/java/com/ssb/financial/account/Account.java
+++ ssbank/src/main/java/com/ssb/financial/account/Account.java
@@ -1,310 +1,262 @@
 package com.ssb.financial.account;
 
-/*
- *  userId   		-- userId(Email)
-	memberIdx       -- 회원번호
-	userPwd        -- 비밀번호
-	createDate      -- 계정생성일
-	lastLogin       -- 마지막 로그인
-	status          -- 계정상태
-	--계좌
-	accountNo  VARCHAR2(16)  NOT NULL, -- 계좌번호
-	memberIdx  NUMBER        NULL,     -- 회원번호
-	branchIdx  NUMBER        NULL,     -- 지점코드
-	productIdx NUMBER        NULL,     -- 상품코드
-	empIdx     VARCHAR2(32)  NULL,     -- 직원코드
-	accountPwd VARCHAR2(100) NULL,     -- 계좌 비밀번호
-	createDate DATE          NULL,     -- 계좌생성일
-	status     NUMBER(1)     NULL      -- 계좌 상태
-	--예금
-	productIdx   NUMBER      NOT NULL, -- 상품코드
-	interestRate NUMBER(5,2) NULL      -- 이율
-	--적금
-	productIdx   NUMBER       NOT NULL, -- 상품코드
-	productType  VARCHAR2(16) NULL,     -- 상품타입
-	entryDate    DATE         NULL,     -- 가입기간
-	monthly      NUMBER       NULL,     -- 월 납입금액
-	interest     NUMBER(4,2)  NULL,     -- 기본 이율
-	vipInterest  NUMBER(4,2)  NULL,     -- 우대이율
-	savingType   VARCHAR2(16) NULL,     -- 저축방법
-	interestType VARCHAR2(16) NULL,     -- 이자지급방식
-	tax          NUMBER(4,2)  NULL      -- 과세
- */
 public class Account {
-	// 예금/적금조회
-	private String accountNo;
-	private int balance;
-	private int branchIdx, productIdx, status, defaultInterest, vipInterest, tax, monthlyAmount;
-	private String productName, productDetail, productType;
-	private String createDate, entryDate, userId, accountPwd, savingType, interestType;
-	private String lastTrDate;
-	// 신규
-	private int memberIdx;
-	private String name,lastName, firstName, birth;
-	private String addr1, addr2,tel;
+	private String lastName;
+	private String firstName;
+	private String name;
+	private String birth;
+	private String addr1;
+	private String addr2;
 	private int zipCode;
-	private double interestRate, interest;
+	private String tel ;
+	private int investScore;
+	private int productIdx;
+	private String productName;
+	private String productDetail;
+	private String createDate;
+	private String productType;
+	private String entryDate;
+	private int monthly;
+	private double interest;
+	private double vipInterest;
+	private String savingType;
+	private String interestType;
+	private double tax;
+	private double interestRate;
+	private String accountNo;
+	private int memberIdx;
+	private int branchIdx;
+	private String empIdx;
+	private String accountPwd;
+	private int status;
+	private int trIdx;
+	private int tType;
+	private int amount;
+	private int balance;
+	private int totBal;
+	private String trDate;
+	private String content;
+	private String LastTrDate;
+	
+
 	
 	
-	
-	
-	
-	
+	public int getTotBal() {
+		return totBal;
+	}
+	public void setTotBal(int totBal) {
+		this.totBal = totBal;
+	}
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	public double getInterestRate() {
-		return interestRate;
-	}
-
-	public void setInterestRate(double interestRate) {
-		this.interestRate = interestRate;
-	}
-
-	public double getInterest() {
-		return interest;
-	}
-
-	public void setInterest(double interest) {
-		this.interest = interest;
-	}
-
-	public String getAccountNo() {
-		return accountNo;
-	}
-
-	public void setAccountNo(String accountNo) {
-		this.accountNo = accountNo;
-	}
-
-	public int getBalance() {
-		return balance;
-	}
-
-	public void setBalance(int balance) {
-		this.balance = balance;
-	}
-
-	public int getBranchIdx() {
-		return branchIdx;
-	}
-
-	public void setBranchIdx(int branchIdx) {
-		this.branchIdx = branchIdx;
-	}
-
-	public int getProductIdx() {
-		return productIdx;
-	}
-
-	public void setProductIdx(int productIdx) {
-		this.productIdx = productIdx;
-	}
-
-	
-	public int getStatus() {
-		return status;
-	}
-
-	public void setStatus(int status) {
-		this.status = status;
-	}
-
-	public String getProductName() {
-		return productName;
-	}
-
-	public void setProductName(String productName) {
-		this.productName = productName;
-	}
-
-	public String getProductDetail() {
-		return productDetail;
-	}
-
-	public void setProductDetail(String productDetail) {
-		this.productDetail = productDetail;
-	}
-
-	public String getCreateDate() {
-		return createDate;
-	}
-
-	public void setCreateDate(String createDate) {
-		this.createDate = createDate;
-	}
-
-	public String getUserId() {
-		return userId;
-	}
-
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-
-	public String getAccountPwd() {
-		return accountPwd;
-	}
-
-	public void setAccountPwd(String accountPwd) {
-		this.accountPwd = accountPwd;
-	}
-
-	public int getDefaultInterest() {
-		return defaultInterest;
-	}
-
-	public void setDefaultInterest(int defaultInterest) {
-		this.defaultInterest = defaultInterest;
-	}
-
-	public int getVipInterest() {
-		return vipInterest;
-	}
-
-	public void setVipInterest(int vipInterest) {
-		this.vipInterest = vipInterest;
-	}
-
-	public int getTax() {
-		return tax;
-	}
-
-	public void setTax(int tax) {
-		this.tax = tax;
-	}
-
-	public int getMonthlyAmount() {
-		return monthlyAmount;
-	}
-
-	public void setMonthlyAmount(int monthlyAmount) {
-		this.monthlyAmount = monthlyAmount;
-	}
-
-	public String getProductType() {
-		return productType;
-	}
-
-	public void setProductType(String productType) {
-		this.productType = productType;
-	}
-
-	public String getEntryDate() {
-		return entryDate;
-	}
-
-	public void setEntryDate(String entryDate) {
-		this.entryDate = entryDate;
-	}
-
-	public String getSavingType() {
-		return savingType;
-	}
-
-	public void setSavingType(String savingType) {
-		this.savingType = savingType;
-	}
-
-	public String getInterestType() {
-		return interestType;
-	}
-
-	public void setInterestType(String interestType) {
-		this.interestType = interestType;
-	}
-
-	public int getMemberIdx() {
-		return memberIdx;
-	}
-
-	public void setMemberIdx(int memberIdx) {
-		this.memberIdx = memberIdx;
-	}
-
 	public String getLastName() {
 		return lastName;
 	}
-
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-
 	public String getFirstName() {
 		return firstName;
 	}
-
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-
 	public String getBirth() {
 		return birth;
 	}
-
 	public void setBirth(String birth) {
 		this.birth = birth;
 	}
-
 	public String getAddr1() {
 		return addr1;
 	}
-
 	public void setAddr1(String addr1) {
 		this.addr1 = addr1;
 	}
-
 	public String getAddr2() {
 		return addr2;
 	}
-
 	public void setAddr2(String addr2) {
 		this.addr2 = addr2;
 	}
-
 	public int getZipCode() {
 		return zipCode;
 	}
-
 	public void setZipCode(int zipCode) {
 		this.zipCode = zipCode;
 	}
-
-	
-
 	public String getTel() {
 		return tel;
 	}
-
 	public void setTel(String tel) {
 		this.tel = tel;
 	}
-
+	public int getInvestScore() {
+		return investScore;
+	}
+	public void setInvestScore(int investScore) {
+		this.investScore = investScore;
+	}
+	public int getProductIdx() {
+		return productIdx;
+	}
+	public void setProductIdx(int productIdx) {
+		this.productIdx = productIdx;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getProductDetail() {
+		return productDetail;
+	}
+	public void setProductDetail(String productDetail) {
+		this.productDetail = productDetail;
+	}
+	public String getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(String createDate) {
+		this.createDate = createDate;
+	}
+	public String getProductType() {
+		return productType;
+	}
+	public void setProductType(String productType) {
+		this.productType = productType;
+	}
+	public String getEntryDate() {
+		return entryDate;
+	}
+	public void setEntryDate(String entryDate) {
+		this.entryDate = entryDate;
+	}
+	public int getMonthly() {
+		return monthly;
+	}
+	public void setMonthly(int monthly) {
+		this.monthly = monthly;
+	}
+	public double getInterest() {
+		return interest;
+	}
+	public void setInterest(double interest) {
+		this.interest = interest;
+	}
+	public double getVipInterest() {
+		return vipInterest;
+	}
+	public void setVipInterest(double vipInterest) {
+		this.vipInterest = vipInterest;
+	}
+	public String getSavingType() {
+		return savingType;
+	}
+	public void setSavingType(String savingType) {
+		this.savingType = savingType;
+	}
+	public String getInterestType() {
+		return interestType;
+	}
+	public void setInterestType(String interestType) {
+		this.interestType = interestType;
+	}
+	public double getTax() {
+		return tax;
+	}
+	public void setTax(double tax) {
+		this.tax = tax;
+	}
+	public double getInterestRate() {
+		return interestRate;
+	}
+	public void setInterestRate(double interestRate) {
+		this.interestRate = interestRate;
+	}
+	public String getAccountNo() {
+		return accountNo;
+	}
+	public void setAccountNo(String accountNo) {
+		this.accountNo = accountNo;
+	}
+	public int getMemberIdx() {
+		return memberIdx;
+	}
+	public void setMemberIdx(int memberIdx) {
+		this.memberIdx = memberIdx;
+	}
+	public int getBranchIdx() {
+		return branchIdx;
+	}
+	public void setBranchIdx(int branchIdx) {
+		this.branchIdx = branchIdx;
+	}
+	public String getEmpIdx() {
+		return empIdx;
+	}
+	public void setEmpIdx(String empIdx) {
+		this.empIdx = empIdx;
+	}
+	public String getAccountPwd() {
+		return accountPwd;
+	}
+	public void setAccountPwd(String accountPwd) {
+		this.accountPwd = accountPwd;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public int getTrIdx() {
+		return trIdx;
+	}
+	public void setTrIdx(int trIdx) {
+		this.trIdx = trIdx;
+	}
+	public int gettType() {
+		return tType;
+	}
+	public void settType(int tType) {
+		this.tType = tType;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	public int getBalance() {
+		return balance;
+	}
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	public String getTrDate() {
+		return trDate;
+	}
+	public void setTrDate(String trDate) {
+		this.trDate = trDate;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
 	public String getLastTrDate() {
-		return lastTrDate;
+		return LastTrDate;
 	}
-
 	public void setLastTrDate(String lastTrDate) {
-		this.lastTrDate = lastTrDate;
+		LastTrDate = lastTrDate;
 	}
-
-	@Override
-	public String toString() {
-		return "Account [accountNo=" + accountNo + ", balance=" + balance + ", branchIdx=" + branchIdx + ", productIdx="
-				+ productIdx + ", status=" + status + ", defaultInterest=" + defaultInterest + ", vipInterest="
-				+ vipInterest + ", tax=" + tax + ", monthlyAmount=" + monthlyAmount + ", productName=" + productName
-				+ ", productDetail=" + productDetail + ", productType=" + productType + ", createDate=" + createDate
-				+ ", entryDate=" + entryDate + ", userId=" + userId + ", accountPwd=" + accountPwd + ", savingType="
-				+ savingType + ", interestType=" + interestType + ", lastTrDate=" + lastTrDate + ", memberIdx="
-				+ memberIdx + ", name=" + name + ", lastName=" + lastName + ", firstName=" + firstName + ", birth="
-				+ birth + ", addr1=" + addr1 + ", addr2=" + addr2 + ", tel=" + tel + ", zipCode=" + zipCode
-				+ ", interestRate=" + interestRate + ", interest=" + interest + "]";
-	}
-
 	
+
 }
--- ssbank/src/main/java/com/ssb/financial/account/AccountController.java
+++ ssbank/src/main/java/com/ssb/financial/account/AccountController.java
@@ -21,109 +21,141 @@ import com.ssb.member.SessionInfo;
 
 @Controller("account.accountController")
 public class AccountController {
-	
+
 	@Autowired
 	private AccountService service;
 	@Autowired
 	private MyExcelView excel;
-	
-	//내가 가지고 있는 예금조회 
-	@RequestMapping(value="/financial/account",method=RequestMethod.GET)
-	public String accountList(HttpSession session,Model model){
-		
-		SessionInfo info =(SessionInfo) session.getAttribute("member");
+
+	// 내가 가지고 있는 예금조회
+	@RequestMapping(value = "/financial/account", method = RequestMethod.GET)
+	public String accountList(HttpSession session, Model model) {
+
+		SessionInfo info = (SessionInfo) session.getAttribute("member");
 		Map<String, Object> map = new HashMap<>();
-		//전체 계좌 가져오기
+		// 전체 계좌 가져오기
 		map.put("memberIdx", info.getMemberIdx());
 		System.out.println(info.getMemberIdx());
-		
+
 		List<Account> deposit = service.depositlistAllAccount(map);
 		List<Account> saving = service.savinglistAllAccount(map);
+		Account deTot = service.deTotalBalance(map);
+		Account saTot = service.saTotalBalance(map);
 		
-		model.addAttribute("deposit",deposit);
-		model.addAttribute("saving",saving);
-		
-		return ".financial.account.fds";
+		//계좌리스트
+		model.addAttribute("deposit", deposit);
+		model.addAttribute("saving", saving);
+		//총 계좌 잔액
+		model.addAttribute("deTot", deTot);
+		model.addAttribute("saTot", saTot);
+
+		return ".financial.account.fds-0001";
 	}
-	
-	//예금 신규
-	@RequestMapping(value="/financial/account/fdn{stage}",method=RequestMethod.GET)
-	public String newAccountForm(@PathVariable String stage,
-			HttpSession session,Model model) {
-		
-		SessionInfo info =(SessionInfo) session.getAttribute("member");
-		//로그인한 멤버 회원 정보 --0002에 이동시 같이 가야 함
+
+	// 입출금 계좌 상세보기
+	@RequestMapping(value = "/financial/account/deDetail")
+	public String deDetail(@RequestParam String accountNo, Model model) {
+		Account dto = null;
+		dto = service.detailDepositAccount(accountNo);
+		System.out.println("controller"+dto.toString());
+		model.addAttribute("dto",dto);
+		return ".financial.account.fds-0002";
+	}
+
+	// 적금계좌
+	@RequestMapping(value = "/financial/account/saDetail")
+	public String saDetail(@RequestParam String accountNo, Model model) {
+		Account dto = null;
+		dto = service.detailSavingAccount(accountNo);
+		model.addAttribute("dto",dto);
+		return ".financial.account.fds-0003";
+	}
+
+	// 예금 신규
+	@RequestMapping(value = "/financial/account/fdn{stage}", method = RequestMethod.GET)
+	public String newAccountForm(@PathVariable String stage, HttpSession session, Model model) {
+
+		SessionInfo info = (SessionInfo) session.getAttribute("member");
+		// 로그인한 멤버 회원 정보 --0002에 이동시 같이 가야 함
 		Account in = service.newAccountMember(info.getMemberIdx());
-		System.out.println(in.toString());
-		if(stage.equals("0002")) {
-		model.addAttribute("info",in);
+		if (stage.equals("0002")) {
+			model.addAttribute("info", in);
 		}
-		return ".financial.account.fdn-"+stage;
+		return ".financial.account.fdn-" + stage;
 	}
-	//신규 입력폼
-	@RequestMapping(value="/financial/account/fdn{stage}",method=RequestMethod.POST)
-	public String newAccountForm(@RequestParam String stage) {
-		
-		return ".financial.account.fdn-"+stage;
-	}
-	@RequestMapping(value="/financial/account/accountNew",method=RequestMethod.POST)
+
+	@RequestMapping(value = "/financial/account/accountNew", method = RequestMethod.POST)
 	public String newAccountSubmit(@RequestParam Account dto) {
-		
+
 		service.insertAccount(dto);
-		
-		//계좌 조회페이지로 이동
+
+		// 계좌 조회페이지로 이동
 		return "redirect: /financial/account/fds";
 	}
-	//해지
-	@RequestMapping(value="/financial/account/accountCancel")
-	public String cancelAccount() {
+
+	// 해지
+	@RequestMapping(value = "/financial/account/cancel{stage}")
+	public String cancelAccount(
+			@RequestParam String stage
+			,HttpSession session
+			,@RequestParam String accountNo,
+			Model model) {
+		
+		SessionInfo info = (SessionInfo) session.getAttribute("member");
+		Map<String, Object> map = new HashMap<>();
+		map.put("memberIdx", info.getMemberIdx());
+		List<Account> account = service.myAccount(map);
+		//Account account = service.;
+		if(stage.equals("0002")) {
+			//해지 조회 페이지 이동 
+		map.put("accountNo", accountNo);
+		Account balance= service.accountBalance(map);
+		model.addAttribute("balance",balance);
+		}
+		//나의 계좌 
+		model.addAttribute("account",account);
+		
 		return ".financial.account.fdc-0001";
 	}
-	//해지조회
-	@RequestMapping(value="/financial/account/accountCheck")
-	public String predictAccount() {
-		return ".financial.account.fdn";
-	}
+
 	
-	
-	//대출 엑셀 다운로드
-	@RequestMapping(value="/financial/account/excel")
-	public View excelDownload(Map<String, Object> model,HttpSession session) {
-		String filename ="account.xls";
+	// 대출 엑셀 다운로드
+	@RequestMapping(value = "/financial/account/excel")
+	public View excelDownload(Map<String, Object> model, HttpSession session) {
+		String filename = "account.xls";
 		String sheetName = "계좌목록";
-		//내려받을 목록리스트
+		// 내려받을 목록리스트
 		List<String> columnLabels = new ArrayList<>();
-		//내려받을 목록값
+		// 내려받을 목록값
 		List<Object[]> columnValues = new ArrayList<>();
-		
-		//내려받을 것들 목룍리스트
+
+		// 내려받을 것들 목룍리스트
 		columnLabels.add("상품명");
 		columnLabels.add("계좌번호");
 		columnLabels.add("이율");
 		columnLabels.add("만기일/최근거래일");
 		columnLabels.add("잔액");
-		
-		
-		//내려받을 값 넣는 처리
-		SessionInfo info =(SessionInfo) session.getAttribute("member");
+
+		// 내려받을 값 넣는 처리
+		SessionInfo info = (SessionInfo) session.getAttribute("member");
 		Map<String, Object> map = new HashMap<>();
 		map.put("memberIdx", info.getMemberIdx());
 		List<Account> deposit = service.depositlistAllAccount(map);
 		List<Account> saving = service.savinglistAllAccount(map);
-		for(Account dep :deposit) {
-			columnValues.add(new Object[] {dep.getProductName(),dep.getAccountNo(),dep.getInterestRate(),
-					dep.getLastTrDate(),dep.getBalance()});
+		for (Account dep : deposit) {
+			columnValues.add(new Object[] { dep.getProductName(), dep.getAccountNo(), dep.getInterestRate(),
+					dep.getLastTrDate(), dep.getBalance() });
 		}
-		for(Account sav :saving) {
-			columnValues.add(new Object[] {sav.getProductName(),sav.getAccountNo(),sav.getInterest(),
-					sav.getLastTrDate(),sav.getBalance()});
+		for (Account sav : saving) {
+			columnValues.add(new Object[] { sav.getProductName(), sav.getAccountNo(), sav.getInterest(),
+					sav.getLastTrDate(), sav.getBalance() });
 		}
-		
+
 		model.put("filename", filename);
 		model.put("sheetName", sheetName);
 		model.put("columnLabels", columnLabels);
 		model.put("columnValues", columnValues);
-		
+
 		return excel;
 	}
 
