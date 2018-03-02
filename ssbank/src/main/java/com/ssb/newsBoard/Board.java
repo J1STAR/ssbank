32588edcc8a48d6e0cf59a8fd7400d4a111a7cef
jeongjong <jong99128@naver.com>
2018-03-02 18:03:48 +0900

    2018-03-02 >> 뉴스, 공지사항 게시판 구현중

---
+++ ssbank/src/main/java/com/ssb/newsBoard/Board.java
@@ -0,0 +1,81 @@
+package com.ssb.newsBoard;
+
+public class Board {
+	private int num, listNum;
+	private String userNum, userId, subject, content, created;
+	private int hitCount;
+	
+	private int replyCount;
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public int getListNum() {
+		return listNum;
+	}
+
+	public void setListNum(int listNum) {
+		this.listNum = listNum;
+	}
+
+	public String getUserNum() {
+		return userNum;
+	}
+
+	public void setUserNum(String userNum) {
+		this.userNum = userNum;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getCreated() {
+		return created;
+	}
+
+	public void setCreated(String created) {
+		this.created = created;
+	}
+
+	public int getHitCount() {
+		return hitCount;
+	}
+
+	public void setHitCount(int hitCount) {
+		this.hitCount = hitCount;
+	}
+
+	public int getReplyCount() {
+		return replyCount;
+	}
+
+	public void setReplyCount(int replyCount) {
+		this.replyCount = replyCount;
+	}
+}
---
+++ ssbank/src/main/webapp/WEB-INF/view/customer/noticeBoard/nbl0001.jsp
@@ -0,0 +1,14 @@
+<%@ page contentType="text/html; charset=UTF-8"%>
+<%@ page trimDirectiveWhitespaces="true" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%
+	String cp=request.getContextPath();
+%>
+<script type="text/javascript">
+	
+</script>
+<div class="alert-info">
+    <span style="font-family: Webdings; font-weight: 600;">m</span>
+     중요한 일정 및 알림, 이벤트 등은 공지사항 통해 고객님께 알려 드립니다.
+</div>
