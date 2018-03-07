34190518e6eee189b14e768a2fcf1ffdd704b29d
kim <kim@DESKTOP-TSKM310>
2018-03-07 12:19:44 +0900

    2018-03-07 고객게시판.컨트롤러.매퍼.jsp 추가

---
+++ ssbank/src/main/java/com/ssb/sale/bbs/Board.java
@@ -0,0 +1,129 @@
+package com.ssb.sale.bbs;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class Board {
+	private int boardIdx, categoryIdx ,fileIdx ;
+	public int getFileIdx() {
+		return fileIdx;
+	}
+	public void setFileIdx(int fileIdx) {
+		this.fileIdx = fileIdx;
+	}
+	private String memberIdx;
+    private int num,listNum;
+    public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	private String userName, userId, subject, content, createDate;
+    private int hitCount;
+    private int replyCount;
+    private int fileSize;
+	public int getFileSize() {
+		return fileSize;
+	}
+	public void setFileSize(int fileSize) {
+		this.fileSize = fileSize;
+	}
+	private String saveFilename;
+	private String originalFilename;
+	private MultipartFile upload; // <input type='file' name='upload' ..
+
+	public int getBoardIdx() {
+		return boardIdx;
+	}
+	public void setBoardIdx(int boardIdx) {
+		this.boardIdx = boardIdx;
+	}
+	public int getCategoryIdx() {
+		return categoryIdx;
+	}
+	public void setCategoryIdx(int categoryIdx) {
+		this.categoryIdx = categoryIdx;
+	}
+	public String getMemberIdx() {
+		return memberIdx;
+	}
+	public void setMemberIdx(String memberIdx) {
+		this.memberIdx = memberIdx;
+	}
+	public String getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(String createDate) {
+		this.createDate = createDate;
+	}
+	private int boardLikeCount;
+	
+	public int getListNum() {
+		return listNum;
+	}
+	public void setListNum(int listNum) {
+		this.listNum = listNum;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getHitCount() {
+		return hitCount;
+	}
+	public void setHitCount(int hitCount) {
+		this.hitCount = hitCount;
+	}
+
+	public String getSaveFilename() {
+		return saveFilename;
+	}
+	public void setSaveFilename(String saveFilename) {
+		this.saveFilename = saveFilename;
+	}
+	public String getOriginalFilename() {
+		return originalFilename;
+	}
+	public void setOriginalFilename(String originalFilename) {
+		this.originalFilename = originalFilename;
+	}
+	public MultipartFile getUpload() {
+		return upload;
+	}
+	public void setUpload(MultipartFile upload) {
+		this.upload = upload;
+	}
+	
+	public int getReplyCount() {
+		return replyCount;
+	}
+	public void setReplyCount(int replyCount) {
+		this.replyCount = replyCount;
+	}
+	public int getBoardLikeCount() {
+		return boardLikeCount;
+	}
+	public void setBoardLikeCount(int boardLikeCount) {
+		this.boardLikeCount = boardLikeCount;
+	}
+}
