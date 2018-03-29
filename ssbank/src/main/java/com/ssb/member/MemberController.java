889942e79e8f91e371ded89b628623829c77e6e1
J1STAR <j.1star.0726@gmail.com>
2018-03-29 17:49:19 +0900

    2018-03-29 PM17:49 >> 회원가입 메일 작업 및 대출 작업

--- ssbank/src/main/java/com/ssb/member/MemberController.java
+++ ssbank/src/main/java/com/ssb/member/MemberController.java
@@ -1,12 +1,17 @@
-	package com.ssb.member;
-
+package com.ssb.member;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,6 +22,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.ssb.velocity.ApplicationConfig;
+import com.ssb.velocity.Mail;
+import com.ssb.velocity.MailService;
 import com.ssb.member.Member;
 
 @Controller("member.memberController")
@@ -29,7 +37,9 @@ public class MemberController {
 	
 	@Autowired
 	private MemberService service;
-	
+
+	@Autowired
+	private MailService mailService;
 
 	@RequestMapping(value="/member/login",method=RequestMethod.GET)
 	public String loginForm(String login_error,Model model, HttpSession session) {
@@ -81,6 +91,8 @@ public class MemberController {
 	@RequestMapping(value="/member/submitOK")
 	public String submitOKForm(HttpSession session, Member dto, @RequestParam String mode, Model model) {
 		
+		dto.setUserName(dto.getLastName() + dto.getFirstName());
+		
 		model.addAttribute("dto", dto);
 		model.addAttribute("mode", mode);
 		
@@ -89,29 +101,100 @@ public class MemberController {
 		return ".member.mbj-0003";
 	}
 	
-	
+	@RequestMapping(value="/member/memberSubmitEmail-{uuid}", method=RequestMethod.GET)
+	public String memberSubmitEmail(@PathVariable String uuid, Model model) {
+		
+		System.out.println("uuid"+uuid);
+		Map<String, Object> map = new HashMap<>();
+		Member dto = new Member();
+		if(uuid == null || uuid.equals("")) {
+			return "redirect:/main";
+		} else {
+			try {
+				map.put("uuid", uuid);
+				String userId = service.memberSubmitEmail(map);
+				System.out.println("userId"+userId);
+				if(userId != null || !userId.equals("")) {
+					dto = service.readMember(userId);
+					dto.setUserName(dto.getLastName() + dto.getFirstName());
+					
+					model.addAttribute("dto", dto);
+					model.addAttribute("mode", "mailOk");
+				} else {
+					return "redirect:/main";
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				
+				return "redirect:/main";
+			}
+			return ".member.mbj-0003";
+		}
+		
+		
+	}
 	
 	
 	/////////////////////////////////////////////////////////////
 	@RequestMapping(value="/member/memberJoinSubmit", method=RequestMethod.POST)
 	@ResponseBody
-	public Map<String, Object> memberSubmit(Member dto) {
+	public Map<String, Object> memberSubmit(HttpServletRequest req, Member dto) {
 		
 		// 패스워드 암호화
 		String hashed = encryptPwd(dto.getUserPwd());
 		dto.setUserPwd(hashed);
+		dto.setUserName(dto.getLastName() + dto.getFirstName());
 		
 		Map<String, Object> model = new HashMap<>();
 		try {
-			service.insertMember(dto);
+			
 			model.put("status", "success");
+			
+			String uuid = UUID.randomUUID().toString();
+			uuid = uuid.replace("-", "");
+			
+			dto.setUuid(uuid);
+			service.insertMember(dto);
+			
+			Mail mail = new Mail();
+	        mail.setMailFrom("ssbank.sist@gmail.com");
+	        mail.setMailTo(dto.getEmail1()+"@"+dto.getEmail2());
+	        mail.setMailSubject("[쌍용은행] "+ dto.getUserName() +" 님의 회원가입을 환영합니다.");
+	 
+	        Map < String, Object > mailModel = new HashMap < String, Object > ();
+	        mailModel.put("firstName", dto.getFirstName());
+	        mailModel.put("lastName", dto.getLastName());
+	        mailModel.put("signature", "www.ssbank.com");
+	        mailModel.put("uuid", uuid);
+	        mail.setModel(mailModel);
+	 
+	        AbstractApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+	        mailService.sendEmail(mail);
+	        context.close();
+
+			/*Mail mailDto = new Mail();
+			mailDto.setSenderEmail("ssbank.sist@gmail.com");
+			mailDto.setSenderName("쌍용은행");
+			mailDto.setReceiverEmail(dto.getEmail1()+"@"+dto.getEmail2());
+			
+			mailDto.setSubject("[쌍용은행] "+ dto.getUserName() +" 님의 회원가입을 환영합니다." );
+			
+			String uuid = UUID.randomUUID().toString();
+			uuid = uuid.replace("-", "");
+			
+			mailDto.setContent("아래 링크를 클릭하여 가입을 완료해주세요."+
+								"<a href='localhost:9090/member/memberSubmitEmail-"+uuid+"'>");
+
+			dto.setUuid(uuid);
+			service.insertMember(dto);
+			mailSender.mailSend(mailDto);*/
 		}catch(Exception e) {
 			model.put("status", "failed");
 		}
 		
 		return model;
 	}
-	
+
 	@RequestMapping(value="/member/memberModiSubmit", method=RequestMethod.POST)
 	@ResponseBody
 	public Map<String, Object> memberModiSubmit(Member dto) {
